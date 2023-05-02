87da62d5633e00ff7565e049ab12a6d6f243f06b
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-05-02 10:51:16 +0500

    Update RandPerson.py

--- RandPerson.py
+++ RandPerson.py
@@ -7,8 +7,14 @@ class RandomPerson(loader.Module):
     strings = {
         "name": "RandPerson",
     }
-    def 
+    def randperson_core(self, message):
+        persones = ["Филард", "Фин", "Анозерггс", "Джекизз", "Ексур", "Яйцелиз Мурино", "Ямадей", "Кальк пдров", "Стакед", "Гг", "Даниила", "Алисбанан", "Варук", "Дебли"]
+        return (
+            '<b>Кто ты из чата Филадея?<b>\n'
+            '\n'
+            f'<b>Ты {rand.choice(persones)}</b>'
+        )
     @loader.command()
     async def randperson(self, message):
         """- Показывает рандомного человека из чата Филадея\nКод спизжен у @yebannq"""
-        await utils.answer(message, self.randmeme(message))
+        await utils.answer(message, self.randperson_core(message))
