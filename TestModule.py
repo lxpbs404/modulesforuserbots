81c39d5828cbe08ef6bfac726719596e1a3526e4
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-05-02 08:50:58 +0500

    Add files via upload

---
+++ TestModule.py
@@ -0,0 +1,14 @@
+from .. import loader, utils
+class TestModule(loader.Module):
+    """Test Mldule by @proshiv_rn9"""
+    
+    strings = {
+        "name": "ModuleExample",
+        "text": "Тестовый текст",
+    }
+    
+    @loader.command()
+    async def example(self, message):
+        """- just for test"""
+        
+        await utils.answer(message, self.strings["text"])
