db553bef71e5eec3e9c8cba45e1b53ba888d996c
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-05-02 09:36:32 +0500

    Add files via upload

---
+++ RandPerson.py
@@ -0,0 +1,21 @@
+from .. import loader, utils
+import random as rand
+__version__ = (1,0,0)
+
+class RandomPerson(loader.Module):
+    """Рандомный человек."""
+    
+    strings = {
+        "name": "RandPerson",
+    }
+    def randmeme_core(self, message):
+        persones = ["Филард", "Фин", "Анозерггс", "Джекизз", "Ексур", "Яйцелиз Мурино", "Ямадей", "Кальк пдров", "Стакед", "Гг", "Даниила"]
+        return (
+            '<b>Кто ты из чата Филадея?<b>\n'
+            '\n'
+            f'<b>Ты {rand.choice(persones)}</b>'
+    @loader.command()
+    async def randperson(self, message):
+        """- Показывает рандомного человека из чата Филадея."""
+        
+        await utils.answer(message, self.randmeme_core(message))
